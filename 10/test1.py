f95c9e0c63ba4dcdc1e6e8ec1c9063fdff35c623
SamSunny <wer567u@163.com>
2015-06-30 15:42:27 +0800

    Tupian

--- 10/test1.py
+++ 10/test1.py
@@ -1,3 +1,4 @@
+#-*- coding:utf-8 –*-
 import MySQLdb as mysql
 from flask import Flask,request,render_template
 import json
@@ -40,9 +41,19 @@ def tupian():
 		"tupian_list":[],
 	}
 	data_id = request.args.get('data_id',0)
+	id_n = request.args.get('id',0)
 	tupian_list=''
-	if int(data_id)==0:
-		sql_tupian='select namery,pather,node_id from tupian order by node_id limit %s,4'  % (data_id*4)
+	sql_count='select count(*) from tupian'
+	cur.execute(sql_count)
+	count=cur.fetchall()[0][0]
+	page_n=(count%4 >0)*1 + count/4
+	if id_n == 'pervious':
+		if int(data_id)!=0:
+			tupian_dict['first_id']=int(data_id)-1
+			tupian_dict['second_id']=int(data_id)-1
+		else:
+			pass
+		sql_tupian='select namery,pather,node_id from tupian order by node_id limit %s,4'  % (tupian_dict['first_id']*4)
 		cur.execute(sql_tupian)
 		sql_tupian_list=cur.fetchall()
 		for x in sql_tupian_list:
@@ -50,15 +61,28 @@ def tupian():
 		tupian_dict['tupian_list']=tupian_list
 		print tupian_list
 		print data_id
-  	else:
-  		sql_tupian='select namery,pather,node_id from tupian order by node_id limit %s,4' % (int(data_id)*4)
+  	elif id_n == 'next':
+  		if int(data_id)!=page_n:
+			tupian_dict['first_id']=int(data_id)
+			tupian_dict['second_id']=int(data_id)+1
+		else:
+			tupian_dict['first_id']=int(data_id)-1
+			tupian_dict['second_id']=int(data_id)
+  		sql_tupian='select namery,pather,node_id from tupian order by node_id limit %s,4' % (tupian_dict['first_id']*4)
 		cur.execute(sql_tupian)
 		sql_tupian_list=cur.fetchall()
 		for x in sql_tupian_list:
 			tupian_list=tupian_list + '''<div class="col-md-3 tupian"><p>No.%s %s</p><img width="100%%" src="%s"></div>'''  % (x[2],x[0],x[1])
 		tupian_dict['tupian_list']=tupian_list
 		print tupian_list
-		print data_id
-	return json.dumps(tupian_list)
+		print tupian_dict['first_id']
+	else:
+		sql_tupian='select namery,pather,node_id from tupian order by node_id limit %s,4' % (int(data_id)*4)
+		cur.execute(sql_tupian)
+		sql_tupian_list=cur.fetchall()
+		for x in sql_tupian_list:
+			tupian_list=tupian_list + '''<div class="col-md-3 tupian"><p>No.%s %s</p><img width="100%%" src="%s"></div>'''  % (x[2],x[0],x[1])
+		tupian_dict['tupian_list']=tupian_list
+	return json.dumps(tupian_dict)
 if __name__ == '__main__':
 	app.run(debug=True)
